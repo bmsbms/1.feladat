6817421061ed327cd88886c30994f70d6938a125
bmsbms <bmsbms.petrik@gmail.com>
2019-09-20 12:51:22 +0200

    hibás

--- 1.feladat/Program.cs
+++ 1.feladat/Program.cs
@@ -10,38 +10,51 @@ namespace _1.feladat
     {
         static void Main(string[] args)
         {
-            int HanyElem;
-            Console.WriteLine("Kérem adjan meg hogy hany darab számot szeretne megadni");
-            HanyElem = Convert.ToInt32(Console.ReadLine());
-            int[] tomb = new int[HanyElem];
+            
+                int HanyElem;
+                Console.WriteLine("Kérem adjan meg hogy hany darab számot szeretne megadni");
+                HanyElem = Convert.ToInt32(Console.ReadLine());
+                int[] tomb = new int[HanyElem];
 
-            Console.WriteLine("Kérem adjon meg {0} db számot",HanyElem);
+
+            
+            if (int.TryParse(Console.ReadLine(), out HanyElem))
+            {
+                Console.WriteLine("Kérem adjon meg {0} db számot", HanyElem);
+
+
+                for (int i = 0; i < tomb.Length; i++)
+                {
+                    tomb[i] = Convert.ToInt32(Console.ReadLine());
+                }
+
+                int legnagyobb = tomb[0];
+                int legkisebb = tomb[0];
+
+                for (int i = 0; i < tomb.Length; i++)
+                {
+                    if (legnagyobb < tomb[i])
+                    {
+                        legnagyobb = tomb[i];
+                    }
+
+                    if (legkisebb > tomb[i])
+                    {
+                        legkisebb = tomb[i];
+                    }
+
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Nem számot adott meg");
+            }
+              
             
 
-            for (int i = 0; i < tomb.Length; i++)
-            {
-                tomb[i] = Convert.ToInt32(Console.ReadLine());
-            }
 
-            int legnagyobb=tomb[0];
-            int legkisebb = tomb[0];
-
-            for (int i = 0; i < tomb.Length; i++)
-            {
-                if (legnagyobb < tomb[i])
-                {
-                    legnagyobb = tomb[i];
-                }
-
-                if (legkisebb> tomb[i])
-                {
-                    legkisebb = tomb[i];
-                }
-
-            }
-
-
-            Console.WriteLine("A legnagyobb szám a bekért adatok közül: {0} a legkisebb pedig {1}",legnagyobb,legkisebb);
+            Console.WriteLine("A legnagyobb szám a bekért adatok közül: {0} a legkisebb pedig {1}", legnagyobb, legkisebb);
 
             Console.ReadKey();
 
